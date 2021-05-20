13b9696e6d1c680f08ccbe521f1d08ab5e632f5a
IlanArellano <ilanarellano15@gmail.com>
2021-05-20 15:06:41 -0500

    Proyecto2

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,56 +1,64 @@
-import React, { useEffect, useRef, useState } from 'react';
-import M from 'materialize-css';
-import { Link } from 'react-router-dom';
-import { pages } from '../helpers/pages';
-import Menu from './Menu';
-import { useLocation } from 'react-router-dom';
-import './styles/nav.css';
+import React, { useEffect, useRef, useState } from "react";
+import M from "materialize-css";
+import { Link } from "react-router-dom";
+import { pages } from "../helpers/pages";
+import Menu from "./Menu";
+import { useLocation } from "react-router-dom";
+import "./styles/nav.css";
 
 const Navbar = () => {
-    const navigator = useRef();
-    const collapse = useRef();
-    const locate = useLocation();
-    const [show, setShow] = useState(false);
+  const newPages = Array.isArray(pages) ? pages.slice(0, 3) : [];
 
-    useEffect(()=>{
-        M.Sidenav.init(navigator.current);
-        M.Collapsible.init(collapse.current);
-        if(locate.pathname === '/proveedores' || 
-         locate.pathname === '/clientes' || 
-         locate.pathname === '/almacen' || 
-         locate.pathname === '/insumos' || 
-         locate.pathname === '/ventas'){
-            setShow(true);
-        } else {
-            setShow(false);
-        }
-    }, [locate.pathname]);
+  const navigator = useRef();
+  const collapse = useRef();
+  const locate = useLocation();
+  const [show, setShow] = useState(false);
 
-    const handleOpen =()=>{
-        const instance  = M.Sidenav.getInstance(navigator.current);
-        instance.open();
+  useEffect(() => {
+    M.Sidenav.init(navigator.current);
+    M.Collapsible.init(collapse.current);
+    if (
+      locate.pathname === "/proveedores" ||
+      locate.pathname === "/clientes" ||
+      locate.pathname === "/almacen" ||
+      locate.pathname === "/insumos" ||
+      locate.pathname === "/ventas"
+    ) {
+      setShow(true);
+    } else {
+      setShow(false);
     }
-    const handleClose = () =>{
-        const instance  = M.Sidenav.getInstance(navigator.current);
-        instance.close();
-    }
-    return (
-        <header>
-        <nav>
-            <div className="nav-wrapper deep-purple darken-3">
-            <div className="brand-logo center"><Link to="/">Electrónica</Link></div>
-             <Menu show={show} handleOpen={handleOpen} />
-            </div>
-                <ul className="sidenav" id="menu" ref={navigator}>
-                    {pages.map((item, index)=>{
-                        return(
-                            <li key={index} onClick={handleClose} ><Link to={item.link}>{item.name}</Link></li>
-                        )
-                    })}
-                </ul>
-        </nav>
-        </header>
-    );
-}
+  }, [locate.pathname]);
+
+  const handleOpen = () => {
+    const instance = M.Sidenav.getInstance(navigator.current);
+    instance.open();
+  };
+  const handleClose = () => {
+    const instance = M.Sidenav.getInstance(navigator.current);
+    instance.close();
+  };
+  return (
+    <header>
+      <nav>
+        <div className="nav-wrapper deep-purple darken-3">
+          <div className="brand-logo center">
+            <Link to="/">Electrónica</Link>
+          </div>
+          <Menu show={show} handleOpen={handleOpen} />
+        </div>
+        <ul className="sidenav" id="menu" ref={navigator}>
+          {newPages.map((item, index) => {
+            return (
+              <li key={index} onClick={handleClose}>
+                <Link to={item.link}>{item.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
